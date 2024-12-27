8e16d28ca9c7f572e6f36c917d87a9c3824d9397
Nate Finch <n8finch@users.noreply.github.com>
2024-12-27 13:58:55 -0600

    Update get-all-posts-endpoint.php

--- get-all-posts-endpoint.php
+++ get-all-posts-endpoint.php
@@ -3,9 +3,12 @@
 class Get_All_Posts_Endpoint
 {
     // Define the path to the JSON file.
-    const ALL_THE_POSTS_JSON_FILE = ABSPATH . 'wp-content/uploads/all-the-posts.json';
+    public $all_posts_file = '';
+
     public function __construct()
     {
+        $upload_dir = wp_upload_dir();
+        $this->all_posts_file = trailingslashit( $upload_dir['basedir'] ) . 'all-the-posts.json';
         // Create a WP REST API endpoint for all the posts.
         add_action('rest_api_init', [ $this, 'create_all_posts_endpoint' ]);
     }
@@ -30,20 +33,22 @@ class Get_All_Posts_Endpoint
     public function get_all_the_posts()
     {
         // Check if the file exists.
-        if (file_exists(self::ALL_THE_POSTS_JSON_FILE) ) {
+        if (file_exists($this->all_posts_file) ) {
+          error_log('FILE is: ' . $this->all_posts_file);
+
             // Check the file timestamp.
-            $file_timestamp = filemtime(self::ALL_THE_POSTS_JSON_FILE);
+            $file_timestamp = filemtime($this->all_posts_file);
             // Check the current timestamp.
             $current_timestamp = time();
             // Check if the file is older than 10 minutes.
             if ($current_timestamp - $file_timestamp < 10 * MINUTE_IN_SECONDS ) {
                   // If the file is less than 10 minutes old, return the contents of the file.
-                  return json_decode(file_get_contents(self::ALL_THE_POSTS_JSON_FILE));
+                  return json_decode(file_get_contents($this->all_posts_file));
             }
         }
 
         // If the file doesn't exist or is older than 10 minutes, create the file.
-        return self::create_all_posts_file();
+        return $this->create_all_posts_file();
     }
 
     /**
@@ -51,52 +56,47 @@ class Get_All_Posts_Endpoint
      *
      * @return object
      */
-    public static function create_all_posts_file()
+    public function create_all_posts_file()
     {
         // Get all the posts.
         $request = wp_remote_post(
-            'https://n8finch2024.local/graphql', [
+            site_url() . '/graphql', [
             'headers' => [
             'Content-Type' => 'application/json',
             ],
             'body' => wp_json_encode(
                 [
                 'query' => '
-								{
-									posts(first: 500) {
-										nodes {
-											id
-											title
-											excerpt(format: RENDERED)
-											date
-											uri
-											featuredImage {
-												node {
-													sourceUrl(size: MEDIUM)
-												}
-											}
-											categories{
-												nodes{
-													id
-													name
-													uri
-												}
-											}
-										}
-									}
-								}
-								'
+			{
+				posts(first: 500) {
+					nodes {
+						id
+						title
+						excerpt(format: RENDERED)
+						date
+						uri
+						featuredImage {
+							node {
+								sourceUrl(size: MEDIUM)
+							}
+						}
+						categories{
+							nodes{
+								id
+								name
+								uri
+							}
+						}
+					}
+				}
+			}
+		'
                 ]
             )
             ]
         );
 
-        // NOTE: the `true` returns the data as an associative
-        // array instead of an object
-        // $decoded_response = json_decode($request['body'], true);
-
-        // Write the events to the file.
-        file_put_contents(self::ALL_THE_POSTS_JSON_FILE, $request['body']);
+        file_put_contents( $this->all_posts_file, $request['body']);
 
         return json_decode($request['body'], true);
     }
